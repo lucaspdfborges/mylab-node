b15459c957401606c26d1266df3db5c9cbfa5c4d
Lucas Borges <lucaspdfborges@gmail.com>
2019-08-21 13:54:22 +0000

    Update req.py

--- req.py
+++ req.py
@@ -2,4 +2,6 @@ import requests
 
 url = 'http://127.0.0.1:8000/hello/'
 headers = {'Authorization': 'Token 9054f7aa9305e012b3c2300408c3dfdf390fcddf'}
-r = requests.get(url, headers=headers)
+params = {'probe_hash': '12567f7bb9305e01fdfc2300408c3dfdf390fadfee'}
+r = requests.get(url, params=params, headers=headers)
+r.text
