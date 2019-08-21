08798db8360500884eccf4b6b4c7b144fc495d4f
Lucas Borges <lucaspdfborges@gmail.com>
2019-08-21 13:54:37 +0000

    Update req.py

--- req.py
+++ req.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:8000/hello/'
+url = 'http://127.0.0.1:8000/api/datum/'
 headers = {'Authorization': 'Token 9054f7aa9305e012b3c2300408c3dfdf390fcddf'}
 params = {'probe_hash': '12567f7bb9305e01fdfc2300408c3dfdf390fadfee'}
 r = requests.get(url, params=params, headers=headers)
