9a53e0361eb22c28372cb4132818e420bc2ed8b8
Live session user <lucaspdfborges@gmail.com>
2019-08-23 16:57:13 +0000

    improving

--- req.py
+++ req.py
@@ -1,7 +1,15 @@
 import requests
+import time
+import math
 
 url = 'http://127.0.0.1:8000/api/datum/'
-headers = {'Authorization': 'Token 9054f7aa9305e012b3c2300408c3dfdf390fcddf'}
-params = {'probe_hash': '12567f7bb9305e01fdfc2300408c3dfdf390fadfee'}
-r = requests.get(url, params=params, headers=headers)
-r.text
+headers = {'Authorization': 'Token 76c563009be5d758e98bf7e89572dad6ea578a28'}
+data = {'probe_hash': 'c41efb16baf6ca11d5ccfec44483d642', 'value': '1', 'note':''}
+
+def funcData(value):
+    return {'probe_hash': 'c41efb16baf6ca11d5ccfec44483d642', 'value': str(value), 'note':''}
+
+for i in range(10):
+    data = funcData(math.pi*i/10)
+    r = requests.post(url, data=data, headers=headers)
+    time.sleep(1)
