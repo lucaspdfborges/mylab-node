c938a14c9f4188c9a5612dd7b62b28b3b8c4cf7e
Live session user <lucaspdfborges@gmail.com>
2019-08-26 10:01:31 +0000

    including get request

--- req.py
+++ req.py
@@ -3,13 +3,21 @@ import time
 import math
 
 url = 'http://127.0.0.1:8000/api/datum/'
-headers = {'Authorization': 'Token 76c563009be5d758e98bf7e89572dad6ea578a28'}
+headers = {'Authorization': 'Token fd191bd59fea5fcb466fc4b0f8a3e04d90e9a42c'}
 data = {'probe_hash': 'c41efb16baf6ca11d5ccfec44483d642', 'value': '1', 'note':''}
 
-def funcData(value):
-    return {'probe_hash': 'c41efb16baf6ca11d5ccfec44483d642', 'value': str(value), 'note':''}
+data2 = {'probe_hash': 'c41efb16baf6ca11d5ccfec44483d642', 'min_value':'3','max_value':'', 'min_timestamp':'', 'max_timestamp':''}
 
-for i in range(10):
-    data = funcData(math.pi*i/10)
-    r = requests.post(url, data=data, headers=headers)
-    time.sleep(1)
+
+def funcData(value):
+    return {'probe_hash': '1a2a1b6e52da93dbcfe4b49cc6a585a0', 'value': str(value), 'note':''}
+
+# for i in range(10):
+#     data = funcData(math.pi*i/10)
+#     r = requests.post(url, data=data, headers=headers)
+#     time.sleep(1)
+
+
+
+r = requests.get(url, params=data2, headers=headers)
+print(r.text)
